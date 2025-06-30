1f9f678d2f013ac0eea462defbc2618a85f42b75
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-06-30 00:12:43 -0300

    Update desafio002.py

--- operadores/desafio002.py
+++ operadores/desafio002.py
@@ -1,14 +1,23 @@
 # PERGUNTANDO O NUMERO DESEJADO
-n = int(input('digite um valor! '))
+n1 = int(input('digite um valor! '))
+n2 = int(input('digite outro valor! '))
 
 # FAZENDO O CALCULO
+q = n1**n2
 
 
+# RESPOSTA
+print('o numero {} ao quadrado é {}!'.format(n1,q))
 
 
-
-
-
+# ---OPERADORES PY---
+# + = ADIÇAO
+# - = SUBTRAÇAO
+# * = MULTIPLICAÇAO
+# / = DIVISAO
+# ** = POTENCIA
+# // = DIVISAO INTEIRA
+# % = RESTO DA DIVISAO
 
 
 
