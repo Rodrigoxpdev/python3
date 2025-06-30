8603e383e0a1b3029f13a7a8a9936657db4beee4
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-06-30 17:27:21 -0300

    desafio 2

--- operadores/desafio002.py
+++ operadores/desafio002.py
@@ -1,13 +1,16 @@
 # PERGUNTANDO O NUMERO DESEJADO
 n1 = int(input('digite um valor! '))
-n2 = int(input('digite outro valor! '))
+# n2 = int(input('digite outro valor! '))
 
 # FAZENDO O CALCULO
-q = n1**n2
+d = n1*2 # MULTIPLICAÇAO
+t = n1*3 # O TRIPLO
+r = n1**(1/2) #RAIZ QUADRADA
+#pow(n1, (1/2)) #RAIZ QUADRADA SEGUNDO MODO DE FAZER
 
 
 # RESPOSTA
-print('o numero {} ao quadrado é {}!'.format(n1,q))
+print('O Dobro de {} vale {} \nO Triplo de {} vale {} \nO numero {} ao quadrado é {:.2f}!'.format(n1, d, n1, t, n1,r ))
 
 
 # ---OPERADORES PY---
